32419f563e272d2e0c0e39b526987554f5fffc2b
edin <vlfdus0401@khu.ac.kr>
2018-01-24 18:07:40 +0900

    subway list API can be 'GET'

--- subway_app/urls.py
+++ subway_app/urls.py
@@ -2,8 +2,10 @@ from . import views
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
+from .views import SubwayListView
 
 urlpatterns = [
     path('', views.subway, name='subway'),
+    path('subway', SubwayListView.as_view(), name='subway'),
     path('select', views.select, name='select')
 ]
--- subway_app/views.py
+++ subway_app/views.py
@@ -1,7 +1,33 @@
 from django.shortcuts import render
 # from django.utils import timezone
 from .models import Subway
-# from rest_framework.response import Response
+from rest_framework import generics, serializers
+from rest_framework.response import Response
+
+
+class SubwayListSerializer(serializers.ModelSerializer):
+
+        class Meta:
+            model = Subway
+            fields = ('TrainNo', 'TrainSttus', 'StationNm', 'StatnTnm')
+
+
+# GET하면 이 listview로 연결
+class SubwayListView(generics.ListAPIView):
+    queryset = Subway.objects.all()
+    serializer_class = SubwayListSerializer
+
+    def list(self, request):
+        queryset = self.get_queryset()
+        serializer_class = self.get_serializer_class()
+        serializer = serializer_class(queryset, many=True)
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        return Response(serializer.data)
 
 
 def subway(request):
