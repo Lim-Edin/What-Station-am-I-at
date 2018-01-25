b4b0a5bd82a3c9fd23abacd3568d4d475ca652e2
edin <vlfdus0401@khu.ac.kr>
2018-01-25 21:57:17 +0900

    modeling but still have an error

--- subway_app/views.py
+++ subway_app/views.py
@@ -1,15 +1,14 @@
-from django.shortcuts import render
-# from django.utils import timezone
 from .models import Subway
 from rest_framework import generics, serializers
 from rest_framework.response import Response
 
 
+# 보여줄 field 명시
 class SubwayListSerializer(serializers.ModelSerializer):
 
         class Meta:
             model = Subway
-            fields = ('TrainNo', 'TrainSttus', 'StationNm', 'StatnTnm')
+            fields = ('id', 'title', 'TrainNo', 'TrainSttus', 'StationNm', 'StatnTnm', 'created_date')
 
 
 # GET하면 이 listview로 연결
@@ -28,12 +27,3 @@ class SubwayListView(generics.ListAPIView):
             return self.get_paginated_response(serializer.data)
 
         return Response(serializer.data)
-
-
-def subway(request):
-    # posts = Posting.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'subway_app/subway.html', {'Subway': Subway})
-
-
-def select(request):
-    return render(request, 'subway_app/select.html')
